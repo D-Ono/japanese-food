049df40a03fc7b575ed0fc84e8d4116bf301cbb9
David Ono <david-ono10@outlook.com>
2023-03-20 20:52:20 -0300

    feat: add header

---
+++ japanese-food-system/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+
+const Header: NextPage = () => {
+    return (
+        <div class="w-full bg-rose-700 h-20  ">
+            <div>Logo</div>
+            <div>Mesa X ou Carrinho</div>
+        </div>
+    );
+};
+
+export default Header;
